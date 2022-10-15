7b61062707d9b3b823445955328258a30c774ad5
manoj616 <113030785+manoj616@users.noreply.github.com>
2022-10-15 10:49:01 +0530

    Create 5.weeke.java

---
+++ Day 4/5.weeke.java
@@ -0,0 +1,71 @@
+import java.util.*;
+public class weeke
+{
+	public int[] orderIndexAscending(int[] array) 
+	{
+        	int[] minimumIndexes = new int[array.length];
+        	int[] sortedArray = array.clone();
+        	Arrays.sort(sortedArray);
+        	for (int index = 0; index < array.length; index++)
+		{
+            	int minIndex = 0;
+            	for (int number : array) 
+			{
+                		if(number == sortedArray[index]) 
+				{ 
+                    		minimumIndexes[index] = minIndex;
+                    		break;
+                		}
+                		minIndex++;
+            	}
+		}
+        	return minimumIndexes;
+    	}
+	public static void main(String[] args)
+	{
+		int i,j,n=5,c;
+		int a[][]=new int[n][n];
+		int x[]=new int[n];
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the matrix: ");
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<n;j++)
+			{
+				a[i][j]=s.nextInt();
+			}
+		}
+		for(i=0;i<n;i++)
+		{
+			c=0;
+			for(j=0;j<n;j++)
+			{
+				if(a[i][j]==1)
+				{
+					c++;
+				}	
+			}
+			System.out.println(i+" Row: "+c);
+			x[i]=c;
+		}
+		/*for(i=0;i<n;i++)
+		{
+			System.out.print(" "+x[i]);
+		}*/
+		int index = 0;
+    		int min = x[index];
+    		for (i = 1; i <n; i++)
+		{
+        		if (x[i] <= min)
+			{
+       			min = x[i];
+        			index = i;
+        		}
+    		}
+		System.out.println("\nWeakest row = "+index);
+		/*sol finder = new sol();
+        	int[] indixes = finder.orderIndexAscending(x);
+        	System.out.println("Indexes of the array in ascending order: " +
+                            Arrays.toString(indixes));*/
+    	}
+}
